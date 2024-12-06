6cec97bc389e7adc6219a68a5fe41d69ba77b13a
Godwinbold <gordonblaze5@gmail.com>
2024-12-06 20:19:34 +0100

    Naming Convention

--- BinCom.Dev/Program.cs
+++ BinCom.Dev/Program.cs
@@ -8,18 +8,19 @@ class Program
 	static void Main(string[] args)
 	{
 		// Task 1: Declaring and initializing a variables and print their values
+
 		string name = "John Doe";
 		int age = 25;
 		bool isAdmin = true;
 
-		Console.WriteLine("Task 1 solution:");
+		Console.WriteLine("Task 1 solution:------------------------------------");
 		Console.WriteLine("Name: " + name);
 		Console.WriteLine("Age: " + age);
 		Console.WriteLine("Is Admin: " + isAdmin);
 		Console.WriteLine();
 
 		// Task 2: If the number is even or odd
-		Console.WriteLine("Task 2:");
+		Console.WriteLine("Task 2:-----------------------------------------------");
 		Console.Write("Enter an integer: ");
 		int userInput = int.Parse(Console.ReadLine());
 		if (userInput % 2 == 0)
@@ -33,7 +34,7 @@ class Program
 		Console.WriteLine();
 
 		// Task 3: Printing numbers 1 to 10 using a for loop
-		
+		Console.WriteLine("Task 3: ----------------------------------------------");
 		for (int i = 1; i <= 10; i++)
 		{
 			Console.WriteLine(i);
@@ -44,7 +45,7 @@ class Program
 		int[] numbers = { 2, 4, 6, 8, 10 };
 		int sum = 0;
 
-		//Console.WriteLine("Task 4:");
+		Console.WriteLine("Task 4:-------------------------------------------------");
 		Console.WriteLine("Array elements:");
 		foreach (int num in numbers)
 		{
@@ -54,14 +55,11 @@ class Program
 		Console.WriteLine("Sum of all elements: " + sum);
 		Console.WriteLine();
 
-		// Task 5: Method to print a greeting
-		//Console.WriteLine("Task 5:");
+
+		// Task 5: Greeting
+		Console.WriteLine("Task 5: ----------------------------------------------");
 		Greet("Alice");
 
-		for(int i = 1;i <= 10; i++)
-		{
-			Console.WriteLine($"{i}");
-		}
 		
 	}
 
@@ -69,6 +67,7 @@ class Program
 	static void Greet(string name)
 	{
 		Console.WriteLine($"Hello, {name}!");
-		
+		Console.ReadLine();
+
 	}
 }
